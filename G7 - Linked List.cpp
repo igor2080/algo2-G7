d8a1edc8848de8a26da7a5dc44aec37050f97022
igor <master20802@gmail.com>
2023-02-08 12:18:59 +0200

    initial commit

---
+++ G7 - Linked List.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+/*******************************************
+
+Igor Alyeksyeyenko, ia22016
+
+G7. Create function to delete from list all elements with bigger value than previous element (before any element is deleted).
+
+Linked List version
+
+Program created: 2023/02/07
+
+*******************************************/
+
+struct Node
+{
+public:
+	int value;
+	Node* next;
+	void push_back(int value);
+	Node(int value) {
+		this->value = value;
+		this->next = NULL;
+	}
+	Node(int values[])
+	{
+		//safe to assume array has at least one element, otherwise other constructor would fire
+		this->value = values[0];
+		this->next = NULL;
+		//insert every passed array value as a new node
+		for (int i = 1; i <= sizeof(values) / sizeof(int); i++)
+		{
+			this->push_back(values[i]);
+		}
+	}
+};
+
+
+void printList(Node* head)
+{
+	Node* current = head;
+	//if the passed list is empty, print nothing
+	if (current != NULL) {
+		//print out the first value outside of loop(for proper formatting)
+		cout << head->value;
+		//go over each remaining node in the list
+		while (current->next != NULL)
+		{
+			//print them out
+			current = current->next;
+			cout << "->";
+			cout << current->value;
+		}
+		//print new line after all of the elements to prevent further input/output on same line
+		cout << endl;
+	}
+}
+
+//insert a value at the end of the linked list
+void Node::push_back(int value)
+{
+	Node* current = this;
+	//find the last item in the list
+	while (current->next != NULL)
+		current = current->next;
+	//add a new node to the end
+	Node* new_node = new Node(value);
+	current->next = new_node;
+}
+
+void removeAllLargerThanPrevious(Node* list)
+{
+	if (!list)//empty/null list passed
+		return;
+
+	Node* current = list;
+	Node* extractedNode;
+	//go over each node
+	while (current->next != NULL)
+	{
+		//if the next node is larger, delete it
+		if (current->value < current->next->value) {
+			extractedNode = current->next;
+			//switch references to the node following the one we're about to delete
+			current->next = current->next->next;
+			//remove references from the deleted node
+			extractedNode->next = NULL;
+			//actually delete it
+			delete extractedNode;
+			extractedNode = NULL;
+		}//only if it is not larger, advance to the next node
+		else {
+			current = current->next;
+		}
+	}
+}
+
+int main()
+{
+	Node* list = new Node(new int[3] {4, 7, 3 });
+	list->push_back(4);
+	printList(list);
+	removeAllLargerThanPrevious(list);
+	printList(list);
+	return 0;
+}
---
+++ G7 - STL LIST.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <list>
+using namespace std;
+/*******************************************
+
+Igor Alyeksyeyenko, ia22016
+
+G7. Create function to delete from list all elements with bigger value than previous element (before any element is deleted).
+
+STL::list version
+
+Program created: 2023/02/07
+
+*******************************************/
+
+void printList(list<int>& list)
+{
+	if (list.empty())
+		return;
+	//print out the first value outside of loop(for proper formatting)
+	cout << list.front();
+	//iterate over each remaining item in the list
+	for (auto i = (++list.begin()); i != list.end(); i++)
+	{
+		cout << "->";
+		cout << *i;
+	}
+	//print new line after all of the elements to prevent further input/output on same line
+	cout << endl;
+}
+
+void removeAllLargerThanPrevious(list<int>& list)
+{
+	//no need to modify empty or lists of one element
+	if (list.empty() || list.size() < 2)
+		return;
+
+	auto item = list.begin();
+	auto adjacent = list.begin();
+	//adjacent will be used for checking for 'larger than previous'
+	++adjacent;
+
+	while (adjacent != list.end())
+	{
+
+		if (*item < *adjacent)
+		{//if the next item is larger, replace it with the next one
+
+			adjacent = list.erase(adjacent);
+
+		}
+		else//only if it is not larger, advance both iterators to the next items
+		{
+			item++;
+			adjacent++;
+		}
+	}
+
+}
+
+int main()
+{
+	list<int> list{ 4,7,3 };
+	list.push_back(4);
+	printList(list);
+	removeAllLargerThanPrevious(list);
+	printList(list);
+	return 0;
+}
